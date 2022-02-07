566147eea3cd2f34e6bcb7b4e9c2b08bafd01a55
Hải Ninh Nguyễn <nninh4647@gmail.com>
2022-02-07 10:08:06 +0700

    Tuan 4

--- src/container/calendar/CalendarPicker.tsx
+++ src/container/calendar/CalendarPicker.tsx
@@ -22,100 +22,82 @@ const CalendarPicker = () => {
   }
 
   const content = (
-    <>
-      <div className='site-calendar-customize-header-wrapper'>
-        <Calendar
-          fullscreen={false}
-          headerRender={({ value, type, onChange, onTypeChange }) => {
-            const start = 0
-            const end = 12
-            const monthOptions = []
+    <div className='site-calendar-customize-header-wrapper'>
+      <Calendar
+        fullscreen={false}
+        headerRender={({ value, type, onChange, onTypeChange }) => {
+          const start = 0
+          const end = 12
+          const monthOptions = []
 
-            const current = value.clone()
-            const localeData = value.localeData()
-            console.log(value.clone())
-            console.log(value.localeData())
+          const current = value.clone()
+          const localeData = value.localeData()
+          console.log(value.clone())
+          console.log(value.localeData())
 
-            const months = []
-            for (let i = start; i < end; i++) {
-              current.month(i)
-              months.push(localeData.monthsShort(current))
-            }
+          const months = []
+          for (let i = start; i < end; i++) {
+            current.month(i)
+            months.push(localeData.monthsShort(current))
+          }
 
-            for (let index = start; index < end; index++) {
-              monthOptions.push(
-                <Select.Option className='month-item' key={`${index}`}>
-                  {months[index]}
-                </Select.Option>
-              )
-            }
-            const month = value.month()
-
-            const year = value.year()
-            const options = []
-            for (let i = year - 10; i < year + 10; i += 1) {
-              options.push(
-                <Select.Option key={i} value={i} className='year-item'>
-                  {i}
-                </Select.Option>
-              )
-            }
-            return (
-              <div style={{ padding: 8 }}>
-                <Row gutter={8}>
-                  <Col>
-                    <LeftOutlined />
-                  </Col>
-
-                  <Col style={{ fontSize: '18px', fontWeight: 'bold' }}>
-                    Tháng {datePicker}
-                  </Col>
-
-                  <Col>
-                    <Select
-                      size='small'
-                      dropdownMatchSelectWidth={false}
-                      value={String(month)}
-                      onChange={(selectedMonth) => {
-                        const newValue = value.clone()
-                        newValue.month(parseInt(selectedMonth, 10))
-                        onChange(newValue)
-                      }}
-                    >
-                      {monthOptions}
-                    </Select>
-                  </Col>
-
-                  <Col>
-                    <RightOutlined />
-                  </Col>
-                </Row>
-
-                <Row>
-                  <Col span={24}>
-                    <Radio.Group
-                      name='radiogroup'
-                      defaultValue={1}
-                      style={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        margin: '14px 0',
-                        gap: '5px',
-                      }}
-                    >
-                      <Radio value={1}>Theo ngày</Radio>
-                      <Radio value={2}>Theo tuần</Radio>
-                    </Radio.Group>
-                  </Col>
-                </Row>
-              </div>
+          for (let index = start; index < end; index++) {
+            monthOptions.push(
+              <Select.Option className='month-item' key={`${index}`}>
+                {months[index]}
+              </Select.Option>
             )
-          }}
-          onPanelChange={onPanelChange}
-        />
-      </div>
-      , mountNode
-    </>
+          }
+          const month = value.month()
+
+          const year = value.year()
+          const options = []
+          for (let i = year - 10; i < year + 10; i += 1) {
+            options.push(
+              <Select.Option key={i} value={i} className='year-item'>
+                {i}
+              </Select.Option>
+            )
+          }
+          return (
+            <div style={{ padding: 8 }}>
+              <Row gutter={8}>
+                <Col>
+                  <LeftOutlined />
+                </Col>
+
+                <Col style={{ fontSize: '18px', fontWeight: 'bold' }}>
+                  Tháng {datePicker}
+                </Col>
+
+                <Col>
+                  <RightOutlined />
+                </Col>
+              </Row>
+
+              <Row>
+                <Col span={24}>
+                  <Radio.Group
+                    name='radiogroup'
+                    defaultValue={1}
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      margin: '14px 0',
+                      gap: '5px',
+                    }}
+                  >
+                    <Radio value={1}>Theo ngày</Radio>
+                    <Radio value={2}>Theo tuần</Radio>
+                  </Radio.Group>
+                </Col>
+              </Row>
+            </div>
+          )
+        }}
+        onPanelChange={onPanelChange}
+      />
+    </div>
   )
 
   return (
